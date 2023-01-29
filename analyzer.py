b1d52c9d399af3f9eaf78155c11fb6472e02c251
nikkitkit007 <nikkitkit0707@mail.ru>
2023-01-29 20:37:24 +0300

    upd analyzer.py doc

--- analyzer.py
+++ analyzer.py
@@ -10,23 +10,44 @@ def analyze():
 
 if __name__ == "__main__":
     """
-    1.0) получаем изображения
-    1.1) получаем классы изображений (CNN)
-    1.2) атакуем изображения и сравниваем с 1.1, получаем процент атакованных изображений с помощью CNN
-    1.3) все атакованные изображения исправляем от искажений с 1.2 и с помощью CNN получаем процент исправленных
+    0) установка параметров эксперимента
+    1) получаем изображение(я) (data)
+    2) сканируем с использованием cnn (cnn)
+    3) проводим атаку на data, получаем процент успешных атак и набор поломанных изображений (img_broken)
+    4) исправляем от искажений img_broken и получаем процент успешных исправлений
     """
-    all_img = 0
+    ###################################################
+    #                       0
+    ###################################################
     img_number = 0
+    all_img = 0
 
     data = Data("photo")
-    # data = Data("cifar")
     cnn = Cnn("mtcnn")
     attacker = Attacker(attack_type="one_px", cnn=cnn)
 
+    ###################################################
+    #                       1
+    ###################################################
     img_data = data.get_img(img_number, all_img)
+    img_broken = []
+
+    ###################################################
+    #                       2
+    ###################################################
     for i in range(data.img_count if all_img == 1 else 1):
         img, meta_img = img_data[i]
-        r = cnn.analyze_img(img)
-        print(r)
-        attacker.attack(img=img, meta=meta_img)
-    pass
+        scan_res = cnn.analyze_img(img)
+        print(scan_res)
+
+        if scan_res:
+            ###################################################
+            #                       3
+            ###################################################
+            attack_res, img_attacked = attacker.attack(img=img, meta=meta_img)
+            print(attack_res)
+            if attack_res:
+                img_broken.append((img_attacked, attack_res))
+
+    print(len(img_broken))
+    print(img_broken)
